5ecf9f97e768e37cf5a842d30ec18d6f4ebc05ce
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-03-11 18:52:16 +0100

    Make the call to the `eyaml` command more robust
    Turns out, an exit code of 0 doesn't mean success - When stdout is
    empty, parse stderr to figure it out

--- ansible/playbooks/filter_plugins/eyaml.py
+++ ansible/playbooks/filter_plugins/eyaml.py
@@ -1,16 +1,28 @@
 """Decrypt secrets using Hiera's EYAML."""
 
-import os
 import subprocess
+
+from ansible.errors import AnsibleFilterError
 
 
 def eyaml(encrypted, keys):
-    output = subprocess.check_output(
-        ["eyaml", "decrypt",
-         "--pkcs7-private-key", keys.priv,
-         "--pkcs7-public-key", keys.pub,
-         "-s", encrypted],
-        stderr=open(os.devnull, 'w'))
+    cmd = ["eyaml", "decrypt",
+           "--pkcs7-private-key", keys['priv'],
+           "--pkcs7-public-key", keys['pub']]
+    proc = subprocess.run(
+        cmd + ["-s", encrypted],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+        check=True)
+
+    # What a nice surprise: eyaml doesn't manage its exit code correctly.
+    output = proc.stdout
+
+    if (not output) and " @ " in proc.stderr:
+        raise AnsibleFilterError("Error running %s: %s" %
+                                 (" ".join(cmd + ["-s", "..."]),
+                                  proc.stderr))
 
     if "\n" not in output.rstrip():
         output = output.rstrip()
