487b6c562b497e9d7d80db9918ecbd0798314d8f
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-04-09 12:47:05 +0200

    Don't recreate Apache certificates if they already exist
    This makes it possible for the development environment to keep them in
    a volume.

--- docker/httpd/docker-entrypoint.sh
+++ docker/httpd/docker-entrypoint.sh
@@ -43,7 +43,9 @@ fi
 /bin/chown www-data: /srv/${WP_ENV}/jahia2wp
 
 /bin/mkdir -p /etc/apache2/ssl
-/usr/bin/openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.cert -subj "/C=CH/ST=Vaud/L=Lausanne/O=Ecole Polytechnique Federale de Lausanne (EPFL)/CN=*.epfl.ch"
+if ! [ -f /etc/apache2/ssl/server.key -a -f /etc/apache2/ssl/server.cert ]; then
+    /usr/bin/openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.cert -subj "/C=CH/ST=Vaud/L=Lausanne/O=Ecole Polytechnique Federale de Lausanne (EPFL)/CN=*.epfl.ch"
+fi
 
 /bin/mkdir -p /var/www/html/probes/ready
 echo "OK" > /var/www/html/probes/ready/index.html
