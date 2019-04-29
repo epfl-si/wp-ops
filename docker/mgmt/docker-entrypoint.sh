190649f57a83a4b3651ec308119a72715dff1c6d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-04-29 10:45:29 +0200

    Manage mgmt and its secrets with Ansible
    * DeploymentConfig copied from the current state in wwp-test
    
    * Couple of additional filter_plugins to do things that really ought
      to come out-of-the-box with Jinja
    
    * Store people's public ssh keys under ansible/vars/ssh-keys.yml
    
    * Create a secret with all required key material; private parts are
      encrypted using
      https://galaxy.ansible.com/epfl_idevelop/ansible_module_eyaml

--- docker/mgmt/docker-entrypoint.sh
+++ docker/mgmt/docker-entrypoint.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-chown -R www-data:www-data /var/www/.ssh
-chmod 0700 /var/www/.ssh
+set -e -x
 
-exec "$@"
+SECRETS_DIR=/var/lib/secrets/ssh
+
+sync_keys() {
+    cp -HL "$SECRETS_DIR"/authorized_keys /var/www/.ssh/
+    chown -R www-data:www-data /var/www/.ssh
+    chmod 0700 /var/www/.ssh
+
+    cp -HL "$SECRETS_DIR"/*key "$SECRETS_DIR"/*key.pub /etc/ssh/
+    chmod 0700 /etc/ssh/*key
+    chown root:root /etc/ssh/*key /etc/ssh/*key.pub
+}
+
+iwait() {
+    if inotifywait "$@"; then return 0; fi
+    local exitcode="$?"
+    case "$exitcode" in
+        0|2) return 0 ;;   # 2 means timeout i.e. nothing happened
+        *) return "$exitcode" ;;
+    esac
+}
+
+################################################
+
+sync_keys
+
+/usr/sbin/sshd -D &
+SSHD_PID=$!
+while iwait -t 300 -r /var/lib/secrets/ssh; do
+    sync_keys
+    kill -HUP $SSHD_PID  # Will fail, and cause the whole script to exit,
+                         # if sshd exited
+done
