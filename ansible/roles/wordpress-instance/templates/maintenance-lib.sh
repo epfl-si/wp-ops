dff432eb9d7c0a612d8007ecd16c686e2438f350
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-05-22 19:26:32 +0200

    enter_maintenance_mode(): don't fail when there is no .htaccess

--- ansible/roles/wordpress-instance/templates/maintenance-lib.sh
+++ ansible/roles/wordpress-instance/templates/maintenance-lib.sh
@@ -5,7 +5,7 @@ enter_maintenance_mode() {
     htaccess="{{ wp_dir }}"/.htaccess
     htaccesstmp="$htaccess"_tmp_$$
     (echo "RewriteRule .* /global-error/303-to-sorryserver.php [L]";
-     grep -v /303-to-sorryserver.php "$htaccess") > "$htaccesstmp"
+     grep -v /303-to-sorryserver.php "$htaccess" || true) > "$htaccesstmp"
     mv "$htaccesstmp" "$htaccess"
 }
 
