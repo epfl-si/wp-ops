8c2a0c8a386104fbb8cb5b0b8668272dbb53ebf4
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-05-22 17:17:15 +0200

    Code and invoke `{enter,leave}_maintenance_mode`
    * Put them in a different template than symlinks-lib.sh as there is
      potential for code re-use in other circumstances than symlinking
    
    * Use an internal redirect to the newly-introduced
      /global-error/303-to-sorryserver.php to effect maintenance mode

---
+++ ansible/roles/wordpress-instance/templates/maintenance-lib.sh
@@ -0,0 +1,18 @@
+# A library of reusable shell functions for maintenance mode
+
+enter_maintenance_mode() {
+    local htaccess htaccesstmp
+    htaccess="{{ wp_dir }}"/.htaccess
+    htaccesstmp="$htaccess"_tmp_$$
+    (echo "RewriteRule .* /global-error/303-to-sorryserver.php [L]";
+     grep -v /303-to-sorryserver.php "$htaccess") > "$htaccesstmp"
+    mv "$htaccesstmp" "$htaccess"
+}
+
+leave_maintenance_mode() {
+    local htaccess htaccesstmp
+    htaccess="{{ wp_dir }}"/.htaccess
+    htaccesstmp="$htaccess"_tmp_$$
+    grep -v /303-to-sorryserver.php "$htaccess" > "$htaccesstmp"
+    mv "$htaccesstmp" "$htaccess"
+}
