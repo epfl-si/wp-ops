b54dfb38c582d0244a1db01ddcaab6450d2efd1d
batKem <demahomirdahk@gmail.com>
2019-08-14 17:06:47 +0200

    python dyn inventory+ mgmt test

---
+++ ansible/roles/wordpress-instance/filter_plugins/kubernetes.py
@@ -0,0 +1,26 @@
+import yaml
+import os
+
+def uri_filter(raw_yaml):
+    
+    #XXX
+    # comments
+    
+    return raw_yaml["spec"]["host"]
+
+def latest_bkp(raw_bash):
+
+    bkps = list(filter(lambda x: "full" in x, raw_bash.split("\n")))
+    dates = list(map( lambda x: x.split("_")[0], bkps ))
+    bkp_archive_name= max(dates)+"_full.tar.gz"
+    bkp_sql_name= max(dates)+"_full.sql"
+    return bkp_archive_name
+    
+    
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'uri_filter':  uri_filter,
+            'latest_bkp': latest_bkp
+        }
