6ac63e33fecadbfdccd9d14c5fac138c01d57440
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-10-10 12:40:31 +0200

    Shell script to create an empty WordPress site in the current directory

---
+++ docker/mgmt/new-wp-site.sh
@@ -0,0 +1,97 @@
+#!/bin/sh
+
+set -e
+
+: ${WORDPRESS_VERSION:=5.2}
+
+usage() {
+    die <<USAGE
+
+Create a symlinked WordPress site into the current directory.
+
+Usage: new-wp-site.sh
+
+USAGE
+
+}
+
+main() {
+    (set -x; wp --path=. core symlink --path_to_version="/wp/$WORDPRESS_VERSION")
+
+    if [ -f wp-config.php ]; then
+        # Retrieve DB credentials from wp-config.php
+        eval "$(perl -ne 'm/(DB_(NAME|USER|PASSWORD)).*, '\''(.*)'\''/ && print lc($1) . "=$3\n";' < wp-config.php)"
+    else
+        db_name="wp_$(mkid 29)"
+        db_user="$(mkid 16)"
+        db_password="$(mkpass 20)"
+        # `wp config create` doesn't care whether the credentials work or not
+        (set -x;
+         wp --path=. config create --dbname="$db_name" --dbuser="$db_user" --dbpass="$db_pass" \
+           --dbhost=db --skip-check)
+    fi
+
+    (set -x; wp db create --dbuser="$MYSQL_SUPER_USER" --dbpass="$MYSQL_SUPER_PASSWORD") || true
+
+    echo "DROP USER '$db_user';" | do_mysql || true
+    do_mysql <<SQL_CREATE_USER
+CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password';
+GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';
+FLUSH PRIVILEGES;
+SQL_CREATE_USER
+}
+
+###############################################################################
+
+die () {
+    if [ -n "$1" ]; then
+        echo >&2 "$@"
+    else
+        cat >&2
+    fi
+    exit 2
+}
+
+check_env() {
+    if [ -z "$MYSQL_SUPER_USER" -o -z "$MYSQL_SUPER_PASSWORD" ]; then
+        die <<MISSING_ENV
+Fatal: either MYSQL_SUPER_USER or MYSQL_SUPER_PASSWORD are unset.
+
+Please source the appropriate .env file and try again.
+
+MISSING_ENV
+    fi
+}
+
+mkpass() {
+    local length=$1
+    pwgen -s "$length" -n 1
+}
+
+mkid() {
+    local length=$1
+    while true; do
+        local id="$(mkpass $length | tr 'A-Z' 'a-z')"
+        case "$id" in
+            [a-z]*) echo "$id"; return 0;;
+        esac
+    done
+}
+
+whine_env() {
+    die <<MESSAGE
+Variable $1 is unset. Please source the appropriate .env file and try again.
+
+MESSAGE
+}
+
+check_env_prereqs() {
+    [ -n "$MYSQL_SUPER_USER" ] || whine_env "MYSQL_SUPER_USER" 
+    [ -n "$MYSQL_SUPER_PASSWORD" ] || whine_env "MYSQL_SUPER_PASSWORD" 
+}
+
+do_mysql() {
+    tee /dev/stderr | (set -x; mysql -h db -u "$MYSQL_SUPER_USER" -p"$MYSQL_SUPER_PASSWORD")
+}
+
+main
