80739c9f1f391c604809dfc35f354e80fc341a48
LuluTchab <l.chaboudez@gmail.com>
2020-02-19 15:41:35 +0100

    Move func to parent class, add property to access some elements

--- ansible/roles/wordpress-instance/action_plugins/wordpress_action_module.py
+++ ansible/roles/wordpress-instance/action_plugins/wordpress_action_module.py
@@ -14,9 +14,164 @@ class WordPressActionModule(ActionBase):
 
         self._tmp = tmp
         self._task_vars = task_vars
+        # Has to be set in child classes with one of the following value:
+        # plugin
+        # mu-plugin
+        # theme
+        self._type = None
+        # Has to be set with element name in child class
+        self._name = None
+
 
         return super(WordPressActionModule, self).run(tmp, task_vars)
 
+    @property
+    def type(self):
+        """
+        Return element type or raise an exception if not initialized
+        """
+        if not self._type:
+            raise ValueError("Please initiliaze 'self._type' in children class {}".format(type(self).__name__))
+            
+        return self._type
+
+    @property
+    def name(self):
+        """
+        Return element name or raise an exception if not initialized
+        """
+        if not self._name:
+            raise ValueError("Please initiliaze 'self._name' in children class {}".format(type(self).__name__))
+            
+        return self._name
+    
+
+    def _get_desired_state(self):
+        """
+        Returns array with installation state and activation state for a (mu-)plugin.
+        We look into YAML given args (plugins.yml)
+        """
+        desired_state = self._task.args.get('state', 'absent')
+        if isinstance(desired_state, six.string_types):
+             desired_state = set([desired_state.strip()])
+        elif isinstance(desired_state, list):
+            desired_state = set(desired_state)
+        else:
+            raise TypeError("Unexpected value for `state`: %s" % desired_state)
+
+        if 'symlinked' in desired_state and 'installed' in desired_state:
+            raise ValueError('% %s cannot be both `symlinked` and `installed`' % self.type, self.name)
+
+        installation_state = self._installation_state(desired_state)
+        activation_state = self._activation_state(desired_state)
+
+        if installation_state == 'absent' and (activation_state == 'active' or self._is_mu):
+            raise ValueError('%s %s cannot be simultaneously absent and %s' %
+                             self.type, self.name, activation_state)
+
+        if activation_state == 'active' or self._is_mu:
+            # Cannot activate (or make a mu-plugin) if not installed
+            if not installation_state:
+                installation_state = 'symlinked'
+        if installation_state == 'absent':
+            # Must (attempt to) deactivate prior to uninstalling
+            if not activation_state:
+                activation_state = "inactive"
+
+        return (installation_state, activation_state)
+    
+
+
+    def _activation_state(self, desired_state):
+        """
+        Returns plugin activation state based on desired state (active, inactive)
+
+        :param desired_state: Plugin desired activation state
+        """
+
+        # Active by default
+        if self._is_mu:
+            return 'active'
+
+        activation_state = desired_state.intersection(['active', 'inactive'])
+        if len(activation_state) == 0:
+            return None
+        elif len(activation_state) == 1:
+            return list(activation_state)[0]
+        else:
+            raise ValueError('%s %s cannot be simultaneously %s' % self.type, self.name, str(list(activation_state)))
+
+
+    def _installation_state(self, desired_state):
+        """
+        Returns plugin installation state based on desired state (present, absent, symlinked)
+
+        :param desired_state: Plugin desired installation state
+        """
+        installation_state = desired_state.intersection(['present', 'absent', 'symlinked'])
+        if len(installation_state) == 0:
+            return None
+        elif len(installation_state) == 1:
+            return list(installation_state)[0]
+        else:
+            raise ValueError('%s %s cannot be simultaneously %s' % self.type, self.name, str(list(installation_state)))
+
+
+    def _do_symlink_file (self, basename):
+        """
+        Creates a symlink for a given plugin file/folder
+
+        :param basename: given plugin file/folder for which we have to create a symlink
+        """
+        return self._run_action('file', {
+            'state': 'link',
+            # Beware src / path inversion, as is customary with everything symlink:
+            'src': self._get_symlink_target(basename),
+            'path': self._get_symlink_path(basename),
+            })
+
+    
+    def _do_rimraf_file (self, basename):
+        """
+        Remove a file/folder belonging to a plugin
+
+        :param basename: given plugin file/folder
+        """
+        self._run_action('file',
+                         {'state': 'absent',
+                          'path': self._get_symlink_path(basename)})
+        return self.result
+
+    
+    def _get_symlink_path (self, basename):
+        """
+        Returns symlink source path
+
+        :param basename: given plugin file/folder for which we want the symlink source path
+        """
+        return self._make_element_path(self._get_wp_dir(), basename)
+
+
+    def _get_symlink_target (self, basename):
+        """
+        Returns a path to symlink target (mu-)plugin or theme file/folder
+
+        :param basename: given plugin file/folder for which we want the symlink target path
+        """
+        return self._make_element_path('../../wp', basename)
+
+
+    def _make_element_path (self, prefix, basename):
+        """
+        Generates an absolute (mu-)plugin or theme path.
+
+        :param prefix: string to add at the beginning of the path to have an absolute one
+        :param basename: given plugin file/folder for which we want the symlink source path
+        """
+        return '%s/wp-content/%ss/%s' % (
+            prefix,
+            self.type,
+            basename)        
 
     def _run_wp_cli_action (self, args):
         """
@@ -29,6 +184,11 @@ class WordPressActionModule(ActionBase):
 
 
     def _run_php_code(self, code):
+        """
+        Execute PHP code and returns result
+
+        :param code: Code to execute
+        """
         result = self._run_shell_action("php -r '%s'" % (code))
 
         return result['stdout_lines']
--- ansible/roles/wordpress-instance/action_plugins/wordpress_plugin.py
+++ ansible/roles/wordpress-instance/action_plugins/wordpress_plugin.py
@@ -19,8 +19,10 @@ class ActionModule(WordPressActionModule):
 
         self.result = super(ActionModule, self).run(tmp, task_vars)
         
-        self._plugin_name = self._task.args.get('name')
+        self._name = self._task.args.get('name')
         self._is_mu = self._task.args.get('is_mu', False)
+
+        self._type = 'mu-plugin' if self._task.args.get('is_mu', False) else 'plugin'
 
         current_activation_state = self._get_plugin_activation_state()
         (desired_installation_state,
@@ -65,7 +67,7 @@ class ActionModule(WordPressActionModule):
         basenames = [os.path.basename(f) for f in froms
                 if self._is_filename(f)]
         if not basenames:
-            basenames = [self._plugin_name]
+            basenames = [self.name]
 
         # Going through each files/folder for plugin
         for basename in basenames:
@@ -93,11 +95,11 @@ class ActionModule(WordPressActionModule):
                                       'not supported (yet)' % basename)
 
         if 'symlinked' in to_undo or 'installed' in to_undo:
-            self._update_result(self._do_rimraf_file(basename, self._is_mu))
+            self._update_result(self._do_rimraf_file(basename))
             if 'failed' in self.result: return self.result
 
         if 'symlinked' in to_do:
-            self._update_result(self._do_symlink_file(basename, self._is_mu))
+            self._update_result(self._do_symlink_file(basename))
             if 'failed' in self.result: return self.result
 
 
@@ -107,7 +109,7 @@ class ActionModule(WordPressActionModule):
 
         :param basename: name of element to check (can be a file or folder)
         """
-        path = self._get_symlink_path(basename, self._is_mu)
+        path = self._get_symlink_path(basename)
         plugin_stat = self._run_action('stat', { 'path': path })
         if 'failed' in plugin_stat:
             raise AnsibleActionFail("Cannot stat() %s" % path)
@@ -116,7 +118,7 @@ class ActionModule(WordPressActionModule):
                 return 'absent'
         elif plugin_stat['stat']['islnk']:
             if (plugin_stat['stat']['lnk_target'] ==
-                self._get_symlink_target(basename, self._is_mu)):
+                self._get_symlink_target(basename)):
                 return 'symlinked'
             else:
                 return 'symlink_damaged'
@@ -124,150 +126,19 @@ class ActionModule(WordPressActionModule):
             return 'installed'
 
 
-    def _get_desired_state(self):
-        """
-        Returns array with installation state and activation state for a (mu-)plugin.
-        We look into YAML given args (plugins.yml)
-        """
-        desired_state = self._task.args.get('state', 'absent')
-        if isinstance(desired_state, six.string_types):
-             desired_state = set([desired_state.strip()])
-        elif isinstance(desired_state, list):
-            desired_state = set(desired_state)
-        else:
-            raise TypeError("Unexpected value for `state`: %s" % desired_state)
-
-        if 'symlinked' in desired_state and 'installed' in desired_state:
-            raise ValueError('Plug-in %s cannot be both `symlinked` and `installed`' % self._plugin_name)
-
-        installation_state = self._installation_state(desired_state)
-        activation_state = self._activation_state(desired_state)
-
-        if installation_state == 'absent' and (activation_state == 'active' or self._is_mu):
-            raise ValueError('Plug-in %s cannot be simultaneously absent and %s' %
-                             self._plugin_name, activation_state)
-
-        if activation_state == 'active' or self._is_mu:
-            # Cannot activate (or make a mu-plugin) if not installed
-            if not installation_state:
-                installation_state = 'symlinked'
-        if installation_state == 'absent':
-            # Must (attempt to) deactivate prior to uninstalling
-            if not activation_state:
-                activation_state = "inactive"
-
-        return (installation_state, activation_state)
-
-
-    def _installation_state(self, desired_state):
-        """
-        Returns plugin installation state based on desired state (present, absent, symlinked)
-
-        :param desired_state: Plugin desired installation state
-        """
-        installation_state = desired_state.intersection(['present', 'absent', 'symlinked'])
-        if len(installation_state) == 0:
-            return None
-        elif len(installation_state) == 1:
-            return list(installation_state)[0]
-        else:
-            raise ValueError('Plug-in cannot be simultaneously %s' % str(list(installation_state)))
-
-
-    def _activation_state(self, desired_state):
-        """
-        Returns plugin activation state based on desired state (active, inactive)
-
-        :param desired_state: Plugin desired activation state
-        """
-
-        # Active by default
-        if self._is_mu:
-            return 'active'
-
-        activation_state = desired_state.intersection(['active', 'inactive'])
-        if len(activation_state) == 0:
-            return None
-        elif len(activation_state) == 1:
-            return list(activation_state)[0]
-        else:
-            raise ValueError('Plug-in %s cannot be simultaneously %s' % str(list(activation_state)))
-
-
-    def _do_symlink_file (self, basename, is_mu):
-        """
-        Creates a symlink for a given plugin file/folder
-
-        :param basename: given plugin file/folder for which we have to create a symlink
-        :param is_mu: Boolean to tell if it's a mu-plugin
-        """
-        return self._run_action('file', {
-            'state': 'link',
-            # Beware src / path inversion, as is customary with everything symlink:
-            'src': self._get_symlink_target(basename, is_mu),
-            'path': self._get_symlink_path(basename, is_mu),
-            })
-
-
-    def _get_symlink_target (self, basename, is_mu):
-        """
-        Returns a path to symlink target plugin file/folder
-
-        :param basename: given plugin file/folder for which we want the symlink target path
-        :param is_mu: Boolean to tell if it's a mu-plugin
-        """
-        return self._make_plugin_path('../../wp', basename, is_mu)
-
 
     def _do_activate_plugin (self):
         """
         Uses WP-CLI to activate plugin
         """
-        return self._run_wp_cli_action('plugin activate %s' % self._plugin_name)
+        return self._run_wp_cli_action('plugin activate %s' % self.name)
 
 
     def _do_deactivate_plugin (self):
         """
         Uses WP-CLI to deactivate plugin
         """
-        return self._run_wp_cli_action('plugin deactivate %s' % self._plugin_name)
-
-
-    def _do_rimraf_file (self, basename, is_mu):
-        """
-        Remove a file/folder belonging to a plugin
-
-        :param basename: given plugin file/folder
-        :param is_mu: Boolean to tell if it's a mu-plugin
-        """
-        self._run_action('file',
-                         {'state': 'absent',
-                          'path': self._get_symlink_path(basename, is_mu)})
-        return self.result
-
-
-    def _get_symlink_path (self, basename, is_mu):
-        """
-        Returns symlink source path
-
-        :param basename: given plugin file/folder for which we want the symlink source path
-        :param is_mu: Boolean to tell if it's a mu-plugin
-        """
-        return self._make_plugin_path(self._get_wp_dir(), basename, is_mu)
-
-
-    def _make_plugin_path (self, prefix, basename, is_mu):
-        """
-        Generates an absolute (mu-)plugin path.
-
-        :param prefix: string to add at the beginning of the path to have an absolute one
-        :param basename: given plugin file/folder for which we want the symlink source path
-        :param is_mu: Boolean to tell if it's a mu-plugin
-        """
-        return '%s/wp-content/%splugins/%s' % (
-            prefix,
-            'mu-' if is_mu else '',
-            basename)
+        return self._run_wp_cli_action('plugin deactivate %s' % self.name)
 
 
     def _get_plugin_activation_state (self):
@@ -283,5 +154,5 @@ class ActionModule(WordPressActionModule):
         for line in result["stdout"].splitlines()[1:]:
             fields = line.split(',')
             if len(fields) < 2: continue
-            if fields[0] == self._plugin_name: return fields[1]
+            if fields[0] == self.name: return fields[1]
         return 'inactive'
