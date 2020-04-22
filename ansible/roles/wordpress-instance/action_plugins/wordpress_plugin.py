0b26379c411233d0732587d1029f1921767813dc
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-04-22 18:51:27 +0200

    [fix] `--check` mode for `-t plugins,themes`
    - Remove all short-circuit code that was turning --check's into skip's
    
    - Plumb an optional `also_in_check_mode` parameter down the whole
    `wordpress_action_module.py`
    
    - Dynamically erase `ansible_check_mode` on actions that have
    `also_in_check_mode=True`

--- ansible/roles/wordpress-instance/action_plugins/wordpress_plugin.py
+++ ansible/roles/wordpress-instance/action_plugins/wordpress_plugin.py
@@ -18,11 +18,6 @@ class ActionModule(WordPressPluginOrThemeActionModule):
     def run (self, tmp=None, task_vars=None):
 
         self.result = super(ActionModule, self).run(tmp, task_vars)
-        
-        # Handling --check execution mode
-        if task_vars['ansible_check_mode']:
-            self.result['skipped'] = True
-            return self.result
 
         self._name = self._task.args.get('name')
         self._mandatory = self._task.args.get('is_mu', False)
