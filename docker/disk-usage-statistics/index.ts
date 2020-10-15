b35ace193aa030a2b1838e9157a6c45286f1c08b
Grégory Charmier <gregory.charmier@gmail.com>
2020-10-15 11:47:15 +0200

    WIP

--- docker/disk-usage-statistics/index.ts
+++ docker/disk-usage-statistics/index.ts
@@ -1,5 +1,28 @@
-import * as fs from 'fs'
+import * as fs from 'fs/promises'
+import { createReadStream } from 'fs'
+import Rx from 'rx'
 
-const parseQdirstat  = async function (file) {
-  const read = fs.createReadStream(file, {encoding: 'utf8'})
+import { readline } from 'readline');
+
+
+async function parseQdirstat (path: string) {  
+  const read = createReadStream(path, {encoding: 'utf8'})
+
+  read.on('data',
+    function (data) {
+      console.log(data)
+      if (data.startsWith("#")) {
+
+      }
+    }
+  )
 }
+
+type Record = {
+  kind: string
+  path: string
+  size: number
+  time: number
+}
+
+parseQdirstat(process.argv[process.argv.length - 1])
