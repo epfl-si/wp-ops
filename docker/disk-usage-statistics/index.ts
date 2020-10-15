83738320528cf25b83d874533ae217ebb4b17ed4
Nicolas Borboën <ponsfrilus@gmail.com>
2020-10-15 14:57:50 +0200

    [beautifullage]

--- docker/disk-usage-statistics/index.ts
+++ docker/disk-usage-statistics/index.ts
@@ -1,51 +1,47 @@
-import lines from "lines-async-iterator";
-import { AsyncIterableX, from } from "ix/asynciterable";
-import { map, flatMap } from "ix/asynciterable/operators";
-import { UnaryFunction } from "ix/interfaces";
+import lines from 'lines-async-iterator'
+import { AsyncIterableX, from } from 'ix/asynciterable'
+import { map, flatMap } from 'ix/asynciterable/operators'
+import { UnaryFunction } from 'ix/interfaces'
 
-function parseQdirstat (path: string) {
+function parseQdirstat(path: string) {
   return from(lines(path)).pipe(
     flatMap((x) => {
-      const parsed = parseLine(x);
-      return from(parsed ? [parsed] : []);
+      const parsed = parseLine(x)
+      return from(parsed ? [parsed] : [])
     })
   )
 }
 
 class Site {
+  public label: string
 
-  public label : string
-
-  static async load () {
-    
-  }
+  static async load() {}
 
   /**
    * Find a Site by the path of one of its files or directories.
-   * 
+   *
    * load() must have succeeded prior.
-   *  
+   *
    * @returns The “best” match, i.e. the Site instance that is lowest
    *          in the filesystem tree and contains `path`.
    */
-  static find (path: String): Site {
-    
-    const s = new Site
-    s.label = "mu"
+  static find(path: String): Site {
+    const s = new Site()
+    s.label = 'mu'
     return s
   }
 }
 
 type Record = {
-  kind: string;
-  path: string;
-  size: number;
-  time: number;
-};
+  kind: string
+  path: string
+  size: number
+  time: number
+}
 
-function parseLine (line: string): Record | undefined {
-  const regexp = /^(.)\s+(.*?)\s+(\d+)\s+(0x[0-9a-f]+)/gm;
-  let matches = regexp.exec(line);
+function parseLine(line: string): Record | undefined {
+  const regexp = /^(.)\s+(.*?)\s+(\d+)\s+(0x[0-9a-f]+)/gm
+  let matches = regexp.exec(line)
   if (matches) {
     return {
       kind: matches[1],
@@ -57,7 +53,10 @@ function parseLine (line: string): Record | undefined {
 }
 
 let pathPrefix
-const qualifyFiles : UnaryFunction<AsyncIterable<Record>, AsyncIterableX<Record>> = map((record) => {
+const qualifyFiles: UnaryFunction<
+  AsyncIterable<Record>,
+  AsyncIterableX<Record>
+> = map((record) => {
   if (record.kind === 'D') {
     pathPrefix = record.path
   }
@@ -68,12 +67,15 @@ const qualifyFiles : UnaryFunction<AsyncIterable<Record>, AsyncIterableX<Record>
 })
 
 const filename = process.argv[process.argv.length - 1]
-const stats : {[k: string] : {count: number}} = {}
-parseQdirstat(filename).pipe(qualifyFiles).forEach((record) => {
-  if (! stats[Site.find(record.path).label]){
-    stats[Site.find(record.path).label] = {count: 0}
-  } 
-  stats[Site.find(record.path).label].count += 1;
-}).then(() => {
-  console.log(stats)
-})
+const stats: { [k: string]: { count: number } } = {}
+parseQdirstat(filename)
+  .pipe(qualifyFiles)
+  .forEach((record) => {
+    if (!stats[Site.find(record.path).label]) {
+      stats[Site.find(record.path).label] = { count: 0 }
+    }
+    stats[Site.find(record.path).label].count += 1
+  })
+  .then(() => {
+    console.log(stats)
+  })
