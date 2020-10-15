a379ec24ff358a00801422de1ca15eeb61e647d4
Nicolas Borboën <ponsfrilus@gmail.com>
2020-10-15 13:36:00 +0200

    [feature] parse line

--- docker/disk-usage-statistics/index.ts
+++ docker/disk-usage-statistics/index.ts
@@ -1,9 +1,7 @@
 import Rx from 'rx'
-
 import LineByLineReader from 'line-by-line'
 
-
-function parseQdirstat (path: string) : Rx.Observable<string> {  
+function parseQdirstat (path: string) : Rx.Observable<string> {
   const rl = new LineByLineReader(path)
 
   return Rx.Observable.fromEvent<string>(rl, 'line')
@@ -17,4 +15,17 @@ type Record = {
   time: number
 }
 
-parseQdirstat(process.argv[process.argv.length - 1]).subscribe((line) => console.log(line))
+function parseLine (line) {
+  const regexp = /^(.)\s+(.*?)\s+(\d+)\s+(0x[0-9a-f]+)/gm;
+  let matches = regexp.exec(line)
+  // console.log(matches)
+  return { kind: 'F', path: 'path', size: 0, time: 0}
+}
+
+parseQdirstat(process.argv[process.argv.length - 1]).subscribe(
+  (line) => {
+    // console.log(line)
+    let parsedLine = parseLine(line)
+    console.log(parsedLine)
+  }
+)
