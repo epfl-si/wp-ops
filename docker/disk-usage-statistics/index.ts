ac4dbef591bba8db0a79584b0eb73b46674c978c
Grégory Charmier <gregory.charmier@gmail.com>
2020-10-15 14:08:53 +0200

    [feature] return real values in parseLine

--- docker/disk-usage-statistics/index.ts
+++ docker/disk-usage-statistics/index.ts
@@ -1,30 +1,36 @@
-import lines from "lines-async-iterator"
-import { from } from 'ix/asynciterable'
+import lines from "lines-async-iterator";
+import { from } from "ix/asynciterable";
 
-import { flatMap } from 'ix/asynciterable/operators'
+import { flatMap } from "ix/asynciterable/operators";
 
-function parseQdirstat (path: string) {
-  return from(lines(path))
-    .pipe(flatMap((x) => {
-      const parsed = parseLine(x)
-      return from(parsed ? [parsed] : [])
-    }))
+function parseQdirstat(path: string) {
+  return from(lines(path)).pipe(
+    flatMap((x) => {
+      const parsed = parseLine(x);
+      return from(parsed ? [parsed] : []);
+    })
+  );
 }
 
 type Record = {
-  kind: string
-  path: string
-  size: number
-  time: number
-}
+  kind: string;
+  path: string;
+  size: number;
+  time: number;
+};
 
-function parseLine (line : string) : Record|undefined {
-  const regexp = /^(.)\s+(.*?)\s+(\d+)\s+(0x[0-9a-f]+)/gm
-  let matches = regexp.exec(line)
+function parseLine(line: string): Record | undefined {
+  const regexp = /^(.)\s+(.*?)\s+(\d+)\s+(0x[0-9a-f]+)/gm;
+  let matches = regexp.exec(line);
   if (matches) {
-    return { kind: 'F', path: 'path', size: 0, time: 0}
+    return {
+      kind: matches[1],
+      path: matches[2],
+      size: Number(matches[3]),
+      time: Number(matches[4]),
+    };
   }
 }
 
-const filename = process.argv[process.argv.length - 1]
-parseQdirstat(filename).forEach(console.log)
+const filename = process.argv[process.argv.length - 1];
+parseQdirstat(filename).forEach(console.log);
