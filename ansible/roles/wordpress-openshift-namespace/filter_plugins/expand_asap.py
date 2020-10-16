9bba46a61282aa251f30c8957112e21733b1de99
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-10-16 15:36:53 +0200

    [feature] Support uppercase ASAP in `-e disk_usage_schedule=ASAP`

--- ansible/roles/wordpress-openshift-namespace/filter_plugins/expand_asap.py
+++ ansible/roles/wordpress-openshift-namespace/filter_plugins/expand_asap.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 def expand_asap(date):
     """Add 2 minutes to crontab if date == asap"""
-    if date == 'asap':
+    if date.lower() == 'asap':
         return (datetime.now() + timedelta(minutes=1)).strftime("%M %H * * *")
     else:
         return date
