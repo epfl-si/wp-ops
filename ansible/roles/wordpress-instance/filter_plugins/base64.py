983d1d9865ceb6fbbe58a5d3364e3903a192d24f
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-06-02 18:26:02 +0200

    [fix] Decode “surrogateescape” monkey business in faux-UTF-8 strings

--- ansible/roles/wordpress-instance/filter_plugins/base64.py
+++ ansible/roles/wordpress-instance/filter_plugins/base64.py
@@ -8,4 +8,13 @@ class FilterModule(object):
             'base64_values': self.base64_values
         }
     def base64_values(self, d):
-        return dict((k, b64encode(bytes(v, 'utf-8'))) for k, v in d.items())
+        return dict((k, b64encode(pry_out_bytes(v)).decode("ascii"))
+                    for k, v in d.items())
+
+def pry_out_bytes(soi_disant_unicode_string_a_la_Python):
+    """YA RLY.
+
+    https://stackoverflow.com/a/27367173/435004
+    """
+    return soi_disant_unicode_string_a_la_Python.encode(
+        'utf-8', 'surrogateescape')
