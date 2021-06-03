9a35a9ff32f38943b48d2265ea0b7d1655563aa0
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-06-03 10:01:45 +0200

    [fix] No more chown's
    As of INC0415927, we no longer have superuser access to NFS. Deal with it

--- docker/httpd/docker-entrypoint.sh
+++ docker/httpd/docker-entrypoint.sh
@@ -12,9 +12,6 @@ fi
 
 /bin/mkdir -p /srv/${WP_ENV}/logs
 
-/bin/chown www-data: /srv/${WP_ENV}
-/bin/chown www-data: /srv/${WP_ENV}/logs
-
 /bin/mkdir -p /etc/apache2/ssl
 if ! [ -f /etc/apache2/ssl/server.key -a -f /etc/apache2/ssl/server.cert ]; then
     /usr/bin/openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.cert -subj "/C=CH/ST=Vaud/L=Lausanne/O=Ecole Polytechnique Federale de Lausanne (EPFL)/CN=*.epfl.ch"
