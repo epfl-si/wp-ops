858e65fd11ee68dc44926da337b6b42455faa83f
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-08-25 14:05:04 +0200

    [proofofconcept] Always serve the same breadcrumb

--- docker/menu-api/index.js
+++ docker/menu-api/index.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/breadcrumb', (req, res) => {
+  const blogname = req.query.blogname,
+        lang = req.query.lang;
+  res.json({ status: "OK", breadcrumb:
+             [{url: "https://example.com", title: "Toto"},
+              {url: "https://example.com", title: "Tutu"}] })
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
