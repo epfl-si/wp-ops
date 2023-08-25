941a5541baa8ca20c1bcf2b5e9333e683856530b
Nicolas Borboën <ponsfrilus@gmail.com>
2023-08-25 15:22:29 +0200

    [test] fake PHP breadcrumb client

---
+++ docker/menu-api/test.php
@@ -0,0 +1,9 @@
+<?php 
+$res = file_get_contents('http://localhost:8888/breadcrumb?lang=fr&bloginfo=https://www.epfl.ch/campus/services/website');
+$res = json_decode($res, false);
+$breadcrumb = '';
+foreach ($res->breadcrumb as $key => $val) {
+  $breadcrumb .= '<a class="breadcrumb" id="breadcrumb_0'.$key.'" href="'.$val->url.'">'.$val->title.'</a>' . ' > ';
+}
+echo substr($breadcrumb, 0, -3);
+?>
