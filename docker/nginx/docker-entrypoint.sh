42f9ef60303de49dd695c9bb3d7c9df168d23f84
Nicolas Borboën <ponsfrilus@gmail.com>
2024-08-19 18:26:22 +0200

    [refactor] embuggable nginx config

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-envsubst '$WP_ENV' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
-
 if ! [ -f /etc/nginx/ssl/server.key -a -f /etc/nginx/ssl/server.cert ]; then
     mkdir -p /etc/nginx/ssl || true
     /usr/bin/openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.cert -subj "/C=CH/ST=Vaud/L=Lausanne/O=Ecole Polytechnique Federale de Lausanne (EPFL)/CN=*.epfl.ch"
