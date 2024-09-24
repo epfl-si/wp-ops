e926d2ba5db6abe776e62df765587800cabf03bf
Azecko <samikarin24@gmail.com>
2024-09-24 09:11:41 +0200

    [fix] Changed the kopf trigger to trigger on `wordpresssites` creation.
    Since we now have a `WordpressSite` kind, we shouldn't have `ephemeralvolumeclaims` but `wordpresssites` for `kopf.on.create()`.

--- kopf-operator/wpn-kopf.py
+++ kopf-operator/wpn-kopf.py
@@ -1,8 +1,7 @@
 # Simple installation of Kopf, source : https://kopf.readthedocs.io/en/latest/walkthrough/starting/
-# WARNING : In the case of this project, it is normal that this code does not do what we want.
 import kopf
 import logging
 
-@kopf.on.create('ephemeralvolumeclaims')
+@kopf.on.create('wordpresssites')
 def create_fn(body, **kwargs):
     logging.info(f"A handler is called with body: {body}")
