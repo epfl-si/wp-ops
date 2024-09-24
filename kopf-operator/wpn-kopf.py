94a3ca1ffb81409ed0c9e0efd68662607bfa036c
Azecko <samikarin24@gmail.com>
2024-09-24 10:37:13 +0200

    [feature] Create Ingress when kind `WordPressSite` is created
    The Ingress is a literal clone from the one in `wpn-nginx.yml`, but created using the Kubernetes Python API.

--- kopf-operator/wpn-kopf.py
+++ kopf-operator/wpn-kopf.py
@@ -1,7 +1,45 @@
-# Simple installation of Kopf, source : https://kopf.readthedocs.io/en/latest/walkthrough/starting/
+# Kopf documentation : https://kopf.readthedocs.io/
 import kopf
-import logging
+from kubernetes import client, config
+
+# Thanks to https://blog.knoldus.com/how-to-create-ingress-using-kubernetes-python-client%EF%BF%BC/
+def create_ingress(networking_v1_api, namespace, name, path):
+    body = client.V1Ingress(
+        api_version="networking.k8s.io/v1",
+        kind="Ingress",
+        metadata=client.V1ObjectMeta(name=name, namespace=namespace),
+        spec=client.V1IngressSpec(
+            rules=[client.V1IngressRule(
+                host="wpn.fsd.team",
+                http=client.V1HTTPIngressRuleValue(
+                    paths=[client.V1HTTPIngressPath(
+                        path=path,
+                        path_type="Prefix",
+                        backend=client.V1IngressBackend(
+                            service=client.V1IngressServiceBackend(
+                                name="wpn-nginx-service",
+                                port=client.V1ServiceBackendPort(
+                                    number=80,
+                                )
+                            )
+                        )
+                    )]
+                )
+            )
+            ]
+        )
+    )
+
+    networking_v1_api.create_namespaced_ingress(
+        namespace=namespace,
+        body=body
+    )
 
 @kopf.on.create('wordpresssites')
-def create_fn(body, **kwargs):
-    logging.info(f"A handler is called with body: {body}")
+def create_fn(spec, name, namespace, logger, **kwargs):
+    
+    path = spec.get('path')
+    config.load_kube_config()
+    networking_v1_api = client.NetworkingV1Api()
+
+    create_ingress(networking_v1_api, namespace, name, path)
