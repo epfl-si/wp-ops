9eb6a7d8c5ef9597f5ba8bc1fdfe60d65d745871
Nicolas Borboën <ponsfrilus@gmail.com>
2024-10-04 15:02:40 +0200

    [refactor] Use of constants
    This add constants for source_dir, domain, theme, MariaDB host and 
    prefixes

--- ensure-wordpress-and-theme.php
+++ ensure-wordpress-and-theme.php
@@ -1,10 +1,37 @@
 <?php
+/**
+ * In order to create the databases, its tables and put some content in it, the
+ * operator need to call this script. This script is basically a minimalistic
+ * "wp cli" that instanciate the WordPress framework and call the same functions
+ * that the installer does, but does not require to have a valid WordPress
+ * installation (a `wp-config.php` and `wp-includes/version.php` as the "wp cli"
+ * does).
+ *
+ * As it's calling WordPress code, the `ABSPATH` constant has to be correctly
+ * defined. It's meant to stand in the root of the wp-dev directory, but feel
+ * free to have it pointing to any directory containing an extract of an
+ * WordPress source. See the __WORDPRESS_SOURCE_DIR constant to change it.
+ *
+ * It also have to be able to connect to the database, so name equivalent to
+ * `DB.wordpress-test.svc` have to be accessible. It means that if you want it
+ * to work from outside the cluster, you have to use something like KubeVPN.
+**/
+
+define( '__WORDPRESS_SOURCE_DIR', '/volumes/wp/6/' );
+define( '__WORDPRESS_DOMAIN', 'wpn.fsd.team' );
+define( '__WORDPRESS_DEFAULT_THEME', 'wp-theme-2018' );
+
+define( '__WP_DB_FQDN', 'mariadb-min.wordpress-test.svc' );
+define( '__WP_DB_NAME_PREFIX', 'wp-db-' );
+define( '__WP_DB_USER_PREFIX', 'wp-db-user-' );
+
 $shortops = "h";
 $longopts  = array(
     "name:",
     "path:",
     "title::",
     "tagline::",
+    "theme::",
 );
 $options = getopt($shortops, $longopts);
 if ( empty($options["name"]) || empty($options["path"]) ) {
@@ -19,22 +46,22 @@ if ( key_exists("h", $options) ) {
   exit();
 }
 define( 'WP_CONTENT_DIR', dirname(__FILE__));
-define( 'ABSPATH', dirname(__FILE__) . '/volumes/wp/6/' );
+define( 'ABSPATH', dirname(__FILE__) . __WORDPRESS_SOURCE_DIR );
 define( 'WP_DEBUG', 1);
 define( 'WP_DEBUG_DISPLAY', 1);
 
 function ensure_clean_site_url($path) {
-  $site_url = "wpn.fsd.team/{$path}/";
+  $site_url = __WORDPRESS_DOMAIN . "/{$path}/";
   return preg_replace('#/+#','/', $site_url);
 }
 define("WP_SITEURL", "https://" . ensure_clean_site_url($options["path"]));
 
-$_SERVER['HTTP_HOST'] = 'wpn.fsd.team';
+$_SERVER['HTTP_HOST'] = __WORDPRESS_DOMAIN;
 
-define("DB_USER", "wp-db-user-{$options["name"]}");
+define("DB_USER", __WP_DB_USER_PREFIX . $options["name"] );
 define("DB_PASSWORD", "secret"); // FIXME
-define("DB_NAME", "wp-db-{$options["name"]}");
-define("DB_HOST", "mariadb-min.wordpress-test.svc");
+define("DB_NAME", __WP_DB_NAME_PREFIX . $options["name"] );
+define("DB_HOST", __WP_DB_FQDN);
 
 global $table_prefix; $table_prefix = "wp_";
 
@@ -81,27 +108,30 @@ function ensure_other_basic_wordpress_things () {
   wp_cache_flush();
 }
 
-function ensure_site_title ($options) {
-  update_option( 'blogname', $options["title"] );
+function ensure_site_title ( $options ) {
+  update_option( 'blogname', stripslashes($options["title"]) );
 }
 
-function ensure_tagline ($options) {
+function ensure_tagline ( $options ) {
   if ( !empty($options["tagline"]) ) {
-    update_option( 'blogdescription', $options["tagline"] );
+    update_option( 'blogdescription', stripslashes($options["tagline"]) );
   }
 }
 
-function ensure_theme ($theme_name) {
+function ensure_theme ( $options ) {
+  if ( empty( $options[ 'theme' ] ) ) {
+    $options[ 'theme' ] = __WORDPRESS_DEFAULT_THEME;
+  }
   global $wp_theme_directories; $wp_theme_directories = [];
-  require_once(ABSPATH . 'wp-includes/theme.php');
+  require_once( ABSPATH . 'wp-includes/theme.php' );
 
-  $theme = wp_get_theme($theme_name);
-  print(switch_theme( $theme->get_stylesheet() ));
+  $theme = wp_get_theme( $options[ 'theme' ] );
+  print( switch_theme( $theme->get_stylesheet() ) );
 }
 
 ensure_db_schema();
 ensure_other_basic_wordpress_things();
 ensure_admin_user("admin", "admin@exemple.com", "secret");
-ensure_site_title($options);
-ensure_tagline($options);
-ensure_theme('wp-theme-2018');
+ensure_site_title( $options );
+ensure_tagline( $options );
+ensure_theme( $options );
