055fb32b7e1e6736d354682b40eca2794ef45de4
Rosa Maggi <rosa.maggi@epfl.ch>
2024-10-18 15:04:52 +0200

    [fix] pydevd-pycharm dependency

--- test/resilience/go.sh
+++ test/resilience/go.sh
@@ -5,7 +5,7 @@ if [ -z "$VIRTUAL_ENV" ] ; then
   [ -d $VENV ] || python3 -m venv $VENV
   source wpresi/bin/activate
 fi
-python3 -m pip install kubernetes requests
+python3 -m pip install kubernetes requests pydevd-pycharm
 
 f=mplp_$(date +%F_%s)
 python3 mariadb-petlepod-downtime.py >$f.out.json 2>$f.err
