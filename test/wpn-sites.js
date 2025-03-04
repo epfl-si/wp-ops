27637af7202b5093b0db58c9da4827c0189b228c
Rosa Maggi <rosa.maggi@epfl.ch>
2025-03-04 18:28:06 +0100

    [fic] rework script

--- test/wpn-sites.js
+++ test/wpn-sites.js
@@ -2,6 +2,7 @@
 var url = require('url');
 
 let namespace = "svc0041p-wordpress"; // svc0041t-wordpress
+let hostname = "www.epfl.ch";
 
 // Function to fetch and filter the data
 async function fetchAndFilterSites() {
@@ -9,14 +10,14 @@ async function fetchAndFilterSites() {
     // Fetch data from the API
     const response = await fetch('https://wp-veritas.epfl.ch/api/v1/sites');
     const data = await response.json();
-
-    // Filter the sites where openshiftEnv is "www" or "labs"
-    // let filteredSites = data.filter(site => site.openshiftEnv !== '');
-    
-    // filter on "labs"
-    let filteredSites = data.filter(site => site.openshiftEnv === 'labs');
+    // filter on "www"
+    let filteredSites = data;
+    filteredSites = filteredSites.filter(site => site.openshiftEnv == 'www' && site.wpInfra);
     // without the WPForms plugin
     filteredSites = filteredSites.filter(site => !site.categories.includes('WPForms'))
+    filteredSites = filteredSites.filter(site => site.ansibleHost.indexOf("www__about") > -1);
+
+    filteredSites.sort((a, b) => b.url.length - a.url.length);
     return filteredSites;
   } catch (error) {
     console.error('Error fetching or filtering sites:', error);
@@ -24,12 +25,18 @@ async function fetchAndFilterSites() {
 }
 
 const determinePlugins = (categories, openshiftEnv) => {
-  if (['www'].indexOf(openshiftEnv) > -1) {
-    categories.push('epfl-menus');
-  }
   return JSON.stringify(categories || [])
 }
 
+const getName = (ansibleHost) => {
+  let name = ansibleHost.replaceAll("__","-").replaceAll("_", "-");
+  let index = 1;
+  while (name.length >= 50) {
+    name = name.split("-").slice(0, index).map(e => e[0]).concat(name.split("-").slice(index)).join("-");
+    index++;
+  }
+  return name;
+}
 
 const run = async () => {
   // Call the function
@@ -41,9 +48,10 @@ const run = async () => {
     const siteYml = `apiVersion: wordpress.epfl.ch/v1
 kind: WordpressSite
 metadata:
-  name: ${site.ansibleHost.replaceAll("__","-").replaceAll("_", "-")}
+  name: ${getName(site.ansibleHost)}
   namespace: ${namespace}
 spec:
+  hostname: ${hostname}
   path: ${path}
   owner:
     epfl:
@@ -62,8 +70,8 @@ spec:
         environment: ${site.openshiftEnv}
         ansibleHost: ${site.ansibleHost}
 `
-    console.log(siteYml);
-    console.log('---');
+      console.log(siteYml);
+      console.log('---');
   }
 }
 
