744edd61acdba3f3f8882c8a841d477d42c19f27
duratti <lindo.duratti@epfl.ch>
2025-05-07 17:19:29 +0200

    List customed objects with wp-cron.py

---
+++ docker/wp-cron/wp-cron.py
@@ -0,0 +1,71 @@
+import time
+from pprint import pprint
+
+from kubernetes import client, config
+from kubernetes.dynamic import DynamicClient
+from kubernetes.client.exceptions import ApiException
+from kubernetes.leaderelection import leaderelection
+from kubernetes.leaderelection.resourcelock.configmaplock import ConfigMapLock
+from kubernetes.leaderelection import electionconfig
+
+
+class classproperty:
+    def __init__(self, func):
+        self.fget = func
+    def __get__(self, instance, owner):
+        return self.fget(owner)
+
+
+class KubernetesAPI:
+  __singleton = None
+
+  @classmethod
+  def __get(cls):
+      if cls.__singleton is None:
+          cls.__singleton = cls()
+
+      return cls.__singleton
+
+  def __init__(self):
+      config.load_config()
+
+      self._custom = client.CustomObjectsApi()
+      self._core = client.CoreV1Api()
+      self._extensions = client.ApiextensionsV1Api()
+      self._dynamic = DynamicClient(client.ApiClient())
+      self._networking = client.NetworkingV1Api()
+
+  @classproperty
+  def custom(cls):
+    return cls.__get()._custom
+
+  @classproperty
+  def core(cls):
+    return cls.__get()._core
+
+  @classproperty
+  def extensions(cls):
+    return cls.__get()._extensions
+
+  @classproperty
+  def dynamic(cls):
+    return cls.__get()._dynamic
+
+  @classproperty
+  def networking(cls):
+    return cls.__get()._networking
+
+
+if __name__ == '__main__':
+    try:
+        group = "wordpress.epfl.ch"
+        version = "v2"
+        namespace = "svc0041t-wordpress"
+        plural = "wordpresssites"
+        api_response = KubernetesAPI.custom.list_namespaced_custom_object(group, version, namespace, plural)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling CustomObjectsApi->list_namespaced_custom_object: %s\n" % e, flush=True)
+    
+    print("All done. I am going to sleep", flush=True)
+    time.sleep(3600)
