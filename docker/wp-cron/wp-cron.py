55b99726064053a9af1fd5be86803e0da4fae5e5
Rosa Maggi <rosa.maggi@epfl.ch>
2025-05-08 11:56:14 +0200

    [feature] Add classes for wordpresssite and ingresses

--- docker/wp-cron/wp-cron.py
+++ docker/wp-cron/wp-cron.py
@@ -54,20 +54,69 @@ class KubernetesAPI:
     return cls.__get()._networking
 
 
+class _BagBase:
+    def __init__(self, items):
+        self._bag = {}
+        for i in items:
+            self.add(i)
+
+    def lookup(self, uid):
+        return self._bag[uid]
+
+    def items(self):
+        return self._bag.items()
+
+    def keys(self):
+        return self._bag.keys()
+
+    def values(self):
+        return self._bag.values()
+
+
+class BagOfWordpressSites (_BagBase):
+    def add(self, wp):
+        uid = wp['metadata']['uid']
+        self._bag[uid] = wp
+
+
+class BagOfIngresses (_BagBase):
+    def add(self, ingress):
+        if (ingress['metadata'].get('ownerReferences')):
+            owner_uid = ingress['metadata']['ownerReferences'][0]['uid']
+            self._bag[owner_uid] = ingress
+
+
+def get_wordpress_sites():
+    group = "wordpress.epfl.ch"
+    version = "v2"
+    namespace = os.getenv('K8S_NAMESPACE')
+    plural = "wordpresssites"
+    api_response = KubernetesAPI.custom.list_namespaced_custom_object(group, version, namespace, plural)
+    return api_response['items']
+
+
+def get_ingresses():
+    group = "networking.k8s.io"
+    version = "v1"
+    namespace = os.getenv('K8S_NAMESPACE')
+    plural = "ingresses"
+    api_response = KubernetesAPI.custom.list_namespaced_custom_object(group, version, namespace, plural)
+    return api_response['items']
+
+
 if __name__ == '__main__':
     try:
-        group = "wordpress.epfl.ch"
-        version = "v2"
-        namespace = os.getenv('K8S_NAMESPACE')
-        plural = "wordpresssites"
-        api_response = KubernetesAPI.custom.list_namespaced_custom_object(group, version, namespace, plural)
-        wordpress_sites = api_response['items']
-        for wp_site in wordpress_sites:
-          wp_site_name = wp_site['metadata']['name']
-          pprint(wp_site_name)
-        
+        ingresses = get_ingresses()
+        wordpresssites = get_wordpress_sites()
+        bag_ingress = BagOfIngresses(ingresses)
+        bag_wp = BagOfWordpressSites(wordpresssites)
+
+        for uid, ingress in bag_ingress.items():
+            wp = bag_wp.lookup(uid)
+            print(f"{ingress['metadata']['name']} -> {wp['metadata']['name']}")
+
     except ApiException as e:
         print("Exception when calling CustomObjectsApi->list_namespaced_custom_object: %s\n" % e, flush=True)
-    
+
     print("All done. I am going to sleep", flush=True)
     time.sleep(3600)
