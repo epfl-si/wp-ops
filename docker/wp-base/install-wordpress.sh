515a840040b653e07789b72069cf577124313798
lvenries <luc.venries@epfl.ch>
2026-09-22 09:25:31 +0200

    [feature] Add block-finder plugin

--- docker/wp-base/install-wordpress.sh
+++ docker/wp-base/install-wordpress.sh
@@ -70,11 +70,13 @@ main () {
     if [ -n "$alpha" ]; then
         install_plugin_wordpress_official polylang
         install_plugin_wordpress_official redirection
+        install_plugin_wordpress_official block-finder
     else
         # The 3.8 polylang version is not anymore compatible with our polylang-cli:
         # - default_language variable is not present anymore
         install_plugin_zip polylang https://downloads.wordpress.org/plugin/polylang.3.7.8.zip
         install_plugin_zip redirection https://downloads.wordpress.org/plugin/redirection.5.5.2.zip
+        install_plugin_zip block-finder https://downloads.wordpress.org/plugin/block-finder.1.1.1.zip 
     fi
 
     # Some of these plugins are commercial plugins that we pay for;
